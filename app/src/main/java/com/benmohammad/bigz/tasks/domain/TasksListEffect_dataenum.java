206b82828d2f200e94d413f31bcd21b6053030dd
Ben Mohammad <benmohammad593@gmail.com>
2020-04-04 12:12:47 +0100

    FeedbackType and TasksListEffect_dataenum added...

---
+++ app/src/main/java/com/benmohammad/bigz/tasks/domain/TasksListEffect_dataenum.java
@@ -0,0 +1,19 @@
+package com.benmohammad.bigz.tasks.domain;
+
+import com.benmohammad.bigz.data.Task;
+import com.google.common.collect.ImmutableList;
+import com.spotify.dataenum.DataEnum;
+import com.spotify.dataenum.dataenum_case;
+
+@DataEnum
+interface TasksListEffect_dataenum {
+
+    dataenum_case RefreshTask();
+    dataenum_case LoadTasks();
+    dataenum_case SaveTask(Task task);
+    dataenum_case DeleteTasks(ImmutableList<Task> tasks);
+    dataenum_case ShowFeedback(FeedbackType feedbackType);
+    dataenum_case NavigateToTaskDetails(Task task);
+    dataenum_case StartTaskCreationFlow();
+
+}
