08e1a837b00ae160504037515b09d168df95e956
Ben Mohammad <benmohammad593@gmail.com>
2020-04-08 12:46:57 +0100

    glitched bugz revamp....

--- app/src/main/java/com/benmohammad/bigz/stats/StatisticsActivity.java
+++ app/src/main/java/com/benmohammad/bigz/stats/StatisticsActivity.java
@@ -1,6 +1,83 @@
 package com.benmohammad.bigz.stats;
 
+import android.bluetooth.le.BluetoothLeScanner;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.NavUtils;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.benmohammad.bigz.R;
+import com.benmohammad.bigz.util.ActivityUtils;
+import com.google.android.material.navigation.NavigationView;
 
 public class StatisticsActivity extends AppCompatActivity {
-}
+
+    private DrawerLayout mDrawerLayout;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.statistics_act);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar ab = getSupportActionBar();
+        ab.setTitle(R.string.statistics_title);
+        ab.setHomeAsUpIndicator(R.drawable.ic_menu);
+        ab.setDisplayHomeAsUpEnabled(true);
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        mDrawerLayout.setStatusBarBackground(R.color.colorPrimaryDark);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        if(navigationView != null) {
+            setupDrawerContent(navigationView);
+        }
+
+        StatisticsFragment statisticsFragment = (StatisticsFragment) getSupportFragmentManager().findFragmentById(R.id.contentFrame);
+        if(statisticsFragment == null) {
+            statisticsFragment = StatisticsFragment.newInstance();
+            ActivityUtils.addFragmentToActivity(getSupportFragmentManager(), statisticsFragment, R.id.contentFrame);
+
+        }
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                menuItem -> {
+                    switch(menuItem.getItemId()) {
+                        case R.id.list_navigation_menu_item:
+                            NavUtils.navigateUpFromSameTask(StatisticsActivity.this);
+                            break;
+                        case R.id.statistics_navigation_menu_item:
+                            break;
+                        default:
+                            break;
+                    }
+
+                    menuItem.setChecked(true);
+                    mDrawerLayout.closeDrawers();
+                    return true;
+                }
+        );
+
+    }}
